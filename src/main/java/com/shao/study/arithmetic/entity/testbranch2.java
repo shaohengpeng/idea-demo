cca51b434c76b098859759715d55958f588f9b0a
shaohengpeng <2973359386@qq.com>
2020-04-29 20:04:44 +0800

    这是利用分支branch2创建的一个测试实体类

---
+++ src/main/java/com/shao/study/arithmetic/entity/testbranch2.java
@@ -0,0 +1,11 @@
+package com.shao.study.arithmetic.entity;
+
+/**
+ * @ClassName testbranch2
+ * @Description TOO
+ * @Author 邵亨鹏
+ * @Date 2020/4/29 20:03
+ * @Version 1.0
+ **/
+public class testbranch2 {
+}
